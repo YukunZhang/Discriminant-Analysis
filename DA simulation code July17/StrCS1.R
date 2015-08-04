685bb65ff092189c6be46590381ed60fbd5a3d21
Yukun Zhang <gemqueenzhang329@gmail.com>
2015-08-04 14:27:15 -0600

    Update and rename StrAR1.R to StrCS1.R

--- DA simulation code July17/StrCS1.R
+++ DA simulation code July17/StrCS1.R
@@ -24,15 +24,7 @@ if(p==3){
     vi1=vi1+t(gp1an)%*%gp1an
   }
 }
-
 vi=vi1/q
-# a2=a[,c(2,2+q,2+2*q)]   #measurement 2
-# a3=a[,c(3,3+q,3+2*q)]    #measurement 3
-# }
-# b1=b[,c(1,4,7)]   #measurement 1
-# b2=b[,c(2,5,8)]   #measurement 2
-# b3=b[,c(3,6,9)]   #measurement 3
-
 
 j0=matrix(0,q,q)
 if (p==3)
@@ -48,9 +40,6 @@ if (p==3)
   j4=cbind(j0,j0,j0,diag(q),j0)
   j5=cbind(j0,j0,j0,j0,diag(q))
 }
-d1=matrix(amu,q,p)
-d2=matrix(bmu,q,p)
-
 
 
 w1=matrix(0,p*q,p*q)
@@ -65,36 +54,21 @@ for (i in 1:n2)
   subb=b[i,]
   w2=w2+t(subb-bmu)%*%(subb-bmu)
 }
-
-
-#### Calculating the initial estimate Ve of V. rest is the common estimate of rho in both the classes####
-rest=(sum(vi)-tr(vi))/(p*(p-1))
-ve=(1-rest^2)*solve((diag(p)+rest^2*c1-rest*c2)) 
-#print (ve)
-#### Calculating the mle's mlv and mlsig of V and sigma respectivaly in population 2####
-mlsige=matrix(0,q,q)
-iter=0
-maxab=1
-ive=solve(ve)
-try=diag(1,p)
-try1=t(try)%*%ive%*%try
-
-if (p==3)
-{delta1=(sum(try1[1,])*d1[,1]+sum(try1[2,])*d1[,2]+sum(try1[3,])*d1[,3])/sum(try1)
- delta2=(sum(try1[1,])*d2[,1]+sum(try1[2,])*d1[,2]+sum(try1[3,])*d2[,3])/sum(try1)
-}else
-{delta1=(sum(try1[1,])*d1[,1]+sum(try1[2,])*d1[,2])/sum(try1)
- delta2=(sum(try1[1,])*d2[,1]+sum(try1[2,])*d1[,2])/sum(try1)
-}
-
-sigma2e=matrix(0,q,q)
-
-
-#  detw=det(w)
+delta1=matrix(apply(t(matrix(amu,q,p)),2,sum)/p,q,1)
+delta2=matrix(apply(t(matrix(bmu,q,p)),2,sum)/p,q,1)
 k1=(t(amu)-one%x%delta1)%*%t(t(amu)-one%x%delta1)
 k2=(t(bmu)-one%x%delta2)%*%t(t(bmu)-one%x%delta2)
 
 w=w1+w2+n1*k1+n2*k2
+
+
+#### Calculating the initial estimate Ve of V. rest is the common estimate of rho in both the classes####
+rest=(sum(vi)-tr(vi))/(p*(p-1))
+ve=(1-rest)*diag(p)+rest*one%*%t(one)
+#### Calculating the mle's mlv and mlsig of V and sigma respectivaly in population 2####
+mlsige=matrix(0,q,q)
+iter=0
+maxab=1
 while ((maxab>converge)&(iter<100))
 {
   ive=solve(ve)
@@ -104,18 +78,18 @@ while ((maxab>converge)&(iter<100))
   for (j in 1:n1)
   {
     subb=a[j,]
-    #     subba=subb-amu
-    #     
-    #     sub1=j1%*%t(subba)
-    #     sub2=j2%*%t(subba)
-    sub1=j1%*%t(t(subb))
-    sub2=j2%*%t(t(subb))
-    sub3=j3%*%t(t(subb))
-    
-    sub1=sub1-delta1
-    sub2=sub2-delta1
-    sub3=sub3-delta1
-    
+#     subba=subb-amu
+#     
+#     sub1=j1%*%t(subba)
+#     sub2=j2%*%t(subba)
+sub1=j1%*%t(t(subb))
+sub2=j2%*%t(t(subb))
+sub3=j3%*%t(t(subb))
+
+sub1=sub1-delta1
+sub2=sub2-delta1
+sub3=sub3-delta1
+
     if (p==3){
       sigma2e=sigma2e+try1[1,1]*(sub1%*%t(sub1))+try1[2,1]*(sub1%*%t(sub2))+try1[3,1]*(sub1%*%t(sub3))+try1[1,2]*(sub2%*%t(sub1))+try1[2,2]*(sub2%*%t(sub2))+try1[3,2]*(sub2%*%t(sub3))+try1[1,3]*(sub3%*%t(sub1))+try1[2,3]*(sub3%*%t(sub2))+try1[3,3]*(sub3%*%t(sub3))
     }else
@@ -144,24 +118,29 @@ while ((maxab>converge)&(iter<100))
     {     sub4=j4%*%t(t(subb))
           sub5=j5%*%t(t(subb))
           sub4=sub4-delta2
-          sub5=sub5-delta2
-          sigma2e=sigma2e+try1[1,1]*(sub1%*%t(sub1))+try1[2,1]*(sub1%*%t(sub2))+try1[3,1]*(sub1%*%t(sub3))+try1[4,1]*(sub1%*%t(sub4))+try1[5,1]*(sub1%*%t(sub5))+try1[1,2]*(sub2%*%t(sub1))+try1[2,2]*(sub2%*%t(sub2))+try1[3,2]*(sub2%*%t(sub3))+try1[4,2]*(sub2%*%t(sub4))+try1[5,2]*(sub2%*%t(sub5))+try1[1,3]*(sub3%*%t(sub1))+try1[2,3]*(sub3%*%t(sub2))+try1[3,3]*(sub3%*%t(sub3))+try1[4,3]*(sub3%*%t(sub4))+try1[5,3]*(sub3%*%t(sub5))+try1[1,4]*(sub4%*%t(sub1))+try1[2,4]*(sub4%*%t(sub2))+try1[3,4]*(sub4%*%t(sub3))+try1[4,4]*(sub4%*%t(sub4))+try1[5,4]*(sub4%*%t(sub5))+try1[1,5]*(sub5%*%t(sub1))+try1[2,5]*(sub5%*%t(sub2))+try1[3,5]*(sub5%*%t(sub3))+try1[4,5]*(sub5%*%t(sub4))+try1[5,5]*(sub5%*%t(sub5))
+       sub5=sub5-delta2
+       sigma2e=sigma2e+try1[1,1]*(sub1%*%t(sub1))+try1[2,1]*(sub1%*%t(sub2))+try1[3,1]*(sub1%*%t(sub3))+try1[4,1]*(sub1%*%t(sub4))+try1[5,1]*(sub1%*%t(sub5))+try1[1,2]*(sub2%*%t(sub1))+try1[2,2]*(sub2%*%t(sub2))+try1[3,2]*(sub2%*%t(sub3))+try1[4,2]*(sub2%*%t(sub4))+try1[5,2]*(sub2%*%t(sub5))+try1[1,3]*(sub3%*%t(sub1))+try1[2,3]*(sub3%*%t(sub2))+try1[3,3]*(sub3%*%t(sub3))+try1[4,3]*(sub3%*%t(sub4))+try1[5,3]*(sub3%*%t(sub5))+try1[1,4]*(sub4%*%t(sub1))+try1[2,4]*(sub4%*%t(sub2))+try1[3,4]*(sub4%*%t(sub3))+try1[4,4]*(sub4%*%t(sub4))+try1[5,4]*(sub4%*%t(sub5))+try1[1,5]*(sub5%*%t(sub1))+try1[2,5]*(sub5%*%t(sub2))+try1[3,5]*(sub5%*%t(sub3))+try1[4,5]*(sub5%*%t(sub4))+try1[5,5]*(sub5%*%t(sub5))
     }
   }
+  
   mlsig=sigma2e/(n*p)
   absig=abs(tr(mlsige-mlsig))
   mlsige=mlsig
   imlsig=solve(mlsig)
-  
-  k5=tr((diag(p)%x%imlsig)%*%w)
-  k6=tr((c1%x%imlsig)%*%w)
-  k7=tr((c2%x%imlsig)%*%w)
+  k3=tr((diag(p)%x%imlsig)%*%w)
+  k4=tr(((one%*%t(one))%x%imlsig)%*%w)
+  #kk3=tr((diag(p)%x%imlsig)%*%w)
+  #mk3=tr((c1%x%imlsig)%*%w)
+  #nk3=tr((c2%x%imlsig)%*%w)
   ####solving the cubic equation ####
   s=p-1
-  pp=(k7)/(-2*n*s*q)
-  qq=(2*n*s*q-2*k5-2*k6)/(-2*n*s*q)
-  rr=k7/(-2*n*s*q)
-  
+  # pp=(nk3)/(-2*n*s*q)
+  # qq=(2*n*s*q-2*kk3-2*mk3)/(-2*n*s*q)
+  # rr=nk3/(-2*n*s*q)
+  ko=n*q*s*p
+  pp=(ko-s*ko+k3*s^2-s*k4)/(s*ko)
+  qq=(2*s*k3-ko)/(s*ko)
+  rr=(k3-k4)/(s*ko)
   aa=(1/3)*(3*qq-pp^2)
   bb=(1/27)*(2*pp^3 -9*pp*qq +27*rr)
   discrim=(bb^2)/4+(aa^3)/27
@@ -175,7 +154,7 @@ while ((maxab>converge)&(iter<100))
                s3=Math.cbrt(ar)}
     
     ro2=s2+s3-pp/3
-    mlv=(1-ro2^2)*solve((diag(p)+ro2^2*c1-ro2*c2)) 
+    mlv=(1-ro2)*diag(p)+ro2*one%*%t(one)
     ve=mlv}
   else{
     dm=sqrt(-discrim)
@@ -190,9 +169,8 @@ while ((maxab>converge)&(iter<100))
     if(rt2>1){rt2=0}
     if(rt3>1){rt3=0}
     ro2=max(rt1,rt2,rt3)
-    mlv=(1-ro2^2)*solve((diag(p)+ro2^2*c1-ro2*c2)) 
+    mlv=(1-ro2)*diag(p)+ro2*one%*%t(one)
     ve=mlv
-    
   }
   iter=iter+1
   abr1=abs(rest-ro2)
